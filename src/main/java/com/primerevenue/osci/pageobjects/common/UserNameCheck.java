5861fb3069246b09dab6a5c8efe3bd4294902166
vsingh01 <vishal.singh@navyuginfo.com>
2016-01-21 01:39:57 +0530

    Hello

---
+++ src/main/java/com/primerevenue/osci/pageobjects/common/UserNameCheck.java
@@ -0,0 +1,29 @@
+package com.primerevenue.osci.pageobjects.common;
+
+import org.apache.log4j.Logger;
+import java.io.File;
+
+//Vishal
+
+
+
+public class UserNameCheck {
+
+	final static Logger logger = Logger.getLogger(UserNameCheck.class);
+	
+	public String UserNameChecks(){
+		
+		
+	String user1;	
+	String username = System.getProperty("user.name");
+	if (username.equalsIgnoreCase("MrCheese-Office"))
+	{
+	  username = "MrCheese";
+	  return username;
+	}
+	else 
+      return username;
+	}
+	
+	
+}
